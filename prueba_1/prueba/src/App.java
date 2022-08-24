9b7c00c9510977b835e699007f8a61ceec45dd1f
adcordova00 <111935488+adcordova00@users.noreply.github.com>
2022-08-23 22:37:19 -0500

    Add files via upload

---
+++ prueba_1/prueba/src/App.java
@@ -0,0 +1,20 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int a = 4;
+        int b = 5;
+        int c = 15;
+
+        suma(a, b, c);
+    }
+
+    private static void suma(int a, int b, int c) {
+
+        int sumar;
+        
+        sumar = a + b + c; 
+        System.out.println(sumar);
+
+    }
+
+}
